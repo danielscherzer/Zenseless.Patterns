aa39e9b1bc89271e80f7b5235999860b05bedc70
Daniel Scherzer <scherzer@rwu.de>
2021-10-03 19:40:39 +0200

    quick commit

---
+++ Tests/RandomExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Zenseless.Patterns.Tests
+{
+	[TestClass()]
+	public class RandomExtensionsTests
+	{
+		[DataTestMethod()]
+		[DataRow(0f, 1f)]
+		[DataRow(-1f, 1f)]
+		[DataRow(-1f, 10f)]
+		[DataRow(-4f, -1f)]
+		public void NextFloatTest(float min, float max)
+		{
+			Random rnd = new();
+			for (int i = 0; i < 100; ++i)
+			{
+				float result = rnd.NextFloat(min, max);
+				Assert.IsTrue(result < max);
+				Assert.IsTrue(min <= result);
+			}
+		}
+	}
+}
---
+++ src/RandomExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zenseless.Patterns
+{
+	/// <summary>
+	/// This static class contains extension methods to the <see cref="Random"/> class for <see cref="float"/> variables.
+	/// </summary>
+	public static class RandomExtensions
+	{
+		/// <summary>
+		/// Returns a random float in the range from <paramref name="min"/>(default = 0) to <paramref name="max"/> default = 1.
+		/// </summary>
+		/// <param name="random">An instance of the <see cref="Random"/> class.</param>
+		/// <param name="min">The inclusive lower bound of the random number returned.</param>
+		/// <param name="max">The exclusive upper bound of the random number returned. <paramref name="max"/> must be greater than or equal to minValue.</param>
+		/// <returns>A 32-bit <see cref="float"/> greater than or equal to <paramref name="min"/> and less than <paramref name="max"/></returns>
+		public static float NextFloat(this Random random, float min = 0f, float max = 1f)
+		{
+			return min + (max - min) * (float)random.NextDouble();
+		}
+	}
+}
