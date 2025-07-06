0900ef71f94bcfc3e0c56939f4f2235a92bf0c82
Daniel Scherzer <scherzer@rwu.de>
2025-07-06 15:17:57 +0200

    quick commit

--- Tests/NoiseTests.cs
+++ Tests/NoiseTests.cs
@@ -1,14 +1,96 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
 
 namespace Zenseless.Patterns.Tests;
 
 [TestClass()]
 public class NoiseTests
 {
+	[TestMethod()]
+	public void ValueTest1dRange()
+	{
+		Random rnd = new(12);
+		Parallel.For(0, 100000, i =>
+		{
+			var x = (float)rnd.NextFloat(-1000000, 1000000);
+			var result = Noise.Value(x);
+			Assert.IsTrue(result >= 0.0f && result <= 1.0f, $"Value out of range for x={x}: {result}");
+		});
+	}
+
+	[DataTestMethod()]
+	[DataRow(-0.005f)]
+	public void ValueTest1dSmooth(float x)
+	{
+		var delta = 0.01f;
+		var x1 = x + delta; // small step to check smoothness
+		var noiseX = Noise.Value(x);
+		var noiseX1 = Noise.Value(x1);
+		var result = Math.Abs(noiseX - noiseX1);
+		Assert.IsTrue(result < delta, $"Output values delta={result} for x={x} and x1={x1}");
+	}
+
+	[TestMethod()]
+	public void ValueTest1dSmoothMany()
+	{
+		Random rnd = new(12);
+		Parallel.For(0, 1000000, i =>
+		{
+			var delta = 0.01f;
+			var x = (float)rnd.NextFloat(-1000, 1000);
+			var x1 = x + delta; // small step to check smoothness
+			var noiseX = Noise.Value(x);
+			var noiseX1 = Noise.Value(x1);
+			var result = Math.Abs(noiseX - noiseX1);
+			Assert.IsTrue(result < 1.5f * delta, $"Output values delta={result} for x={x} and x1={x1}");
+		});
+	}
+
+	[DataTestMethod()]
+	[DataRow(0, 0.03f)]
+	public void ValueTest1d(float x, float expected)
+	{
+		var delta = 0.01f;
+		var result = Noise.Value(x);
+		Assert.AreEqual(expected, result, delta);
+	}
+
+	[TestMethod()]
+	public void ValueTest2dRange()
+	{
+		Random rnd = new(12);
+		Parallel.For(0, 1000, i =>
+		{
+			var x = (float)rnd.NextFloat(-1000000, 1000000);
+			var y = (float)rnd.NextFloat(-1000000, 1000000);
+			var result = Noise.Value(x, y);
+			Assert.IsTrue(result >= 0.0f && result <= 1.0f, $"Value out of range for x={x} y={y}: {result}");
+		});
+	}
+
+	[TestMethod()]
+	public void ValueTest2dSmooth()
+	{
+		Random rnd = new(12);
+		Parallel.For(0, 1000000, i =>
+		{
+			var delta = 0.01f;
+			var x = (float)rnd.NextFloat(-1000, 1000);
+			var y = (float)rnd.NextFloat(-1000, 1000);
+			var x1 = x + delta; // small step to check smoothness
+			var y1 = y + delta; // small step to check smoothness
+			var noiseX = Noise.Value(x, y);
+			var noiseX1 = Noise.Value(x1, y1);
+			var result = Math.Abs(noiseX - noiseX1);
+			Assert.IsTrue(result < delta * 10, $"Output values delta={result} for x={x}: ");
+		});
+	}
+
 	[DataTestMethod()]
 	[DataRow(0, 0, 0.36f)]
 	[DataRow(0, 1, 0.97f)]
-	public void ValueTest(float x, float y, float expected)
+	public void ValueTest2d(float x, float y, float expected)
 	{
 		var delta = 0.01f;
 		var result = Noise.Value(x, y);
--- src/Noise.cs
+++ src/Noise.cs
@@ -1,5 +1,6 @@
 ﻿namespace Zenseless.Patterns;
 
+using System;
 using static MathHelper;
 
 /// <summary>
@@ -29,6 +30,30 @@ public static class Noise
 	/// <returns>A 32-bit unsigned integer representing the calculated seed value.</returns>
 	public static uint Seed(uint x, uint y) => (19u * x) + (47u * y) + 101u;
 
+
+	/// <summary>
+	/// Computes a smoothly interpolated value noise based on the input.
+	/// </summary>
+	/// <remarks>This method generates a value by performing cubic interpolation between random values at the
+	/// nearest integer points surrounding the input. The interpolation is weighted using a smoothstep
+	/// function to ensure smooth transitions between values.</remarks>
+	/// <param name="x">The input position.</param>
+	/// <returns>A floating-point value in the range [0..1] representing the interpolated result at the specified input.</returns>
+	public static float Value(float x)
+	{
+		static float Hash(uint x) => (float)Pcg(x) / 0xffffffffU;
+		
+		float xMin = MathF.Floor(x);
+		uint ux = (uint)xMin; // unsigned integer position
+
+		//random value at surrounding integer positions
+		float x0 = Hash(ux);
+		float x1 = Hash(ux + 1);
+
+		float weight = Smoothstep(0.0f, 1.0f, x - xMin); // cubic interpolation
+		return Lerp(x0, x1, weight);
+	}
+
 	/// <summary>
 	/// Computes a smoothly interpolated value noise based on the input coordinates.
 	/// </summary>
@@ -40,8 +65,16 @@ public static class Noise
 	/// <returns>A floating-point value in the range [0..1] representing the interpolated result at the specified coordinates.</returns>
 	public static float Value(float x, float y)
 	{
-		uint ux = (uint)x.FastTruncate(); // unsigned integer position
-		uint uy = (uint)y.FastTruncate();
+		static float Hash(uint x, uint y)
+		{
+			uint a = Pcg(Seed(x, y));
+			return (float)a / 0xffffffffU;
+		}
+
+		float xMin = MathF.Floor(x);
+		float yMin = MathF.Floor(y);
+		uint ux = (uint)xMin; // unsigned integer position
+		uint uy = (uint)yMin;
 
 		//random value at surrounding integer positions
 		float v00 = Hash(ux, uy);
@@ -49,18 +82,12 @@ public static class Noise
 		float v01 = Hash(ux, uy + 1);
 		float v11 = Hash(ux + 1, uy + 1);
 
-		float weightX = Smoothstep(0.0f, 1.0f, x.Fract()); // cubic interpolation
+		float weightX = Smoothstep(0.0f, 1.0f, x - xMin); // cubic interpolation
 
 		float x1 = Lerp(v00, v10, weightX);
 		float x2 = Lerp(v01, v11, weightX);
 		
-		float weightY = Smoothstep(0.0f, 1.0f, y.Fract()); // cubic interpolation
+		float weightY = Smoothstep(0.0f, 1.0f, y - yMin); // cubic interpolation
 		return Lerp(x1, x2, weightY);
-
-	}
-	private static float Hash(uint x, uint y)
-	{
-		uint a = Pcg(Seed(x, y));
-		return (float)a / 0xffffffffU;
 	}
 }
